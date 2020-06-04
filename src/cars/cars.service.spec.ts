7d9c4458b1b8a5b49e346bd6474520ca5b231677
sigfried <rssanjuan2704@gmail.com>
2020-06-04 18:52:20 -0500

    fix(test): Fix test due to changes

--- src/cars/cars.service.spec.ts
+++ src/cars/cars.service.spec.ts
@@ -6,35 +6,35 @@ import { CarsRepository } from './repositories/base/cars-repository';
 import { OwnersRepository } from './repositories/base/owners-repository';
 
 import { mockOwner, mockCar, mockManufacturer } from './utils/test/mocks';
+import { Car, Manufacturer } from './schemas';
 
 const carsMockArray = [
   mockCar(),
-  mockCar(new Date('2019-05-05'), 'Ford Motors', ['Ruben Garcia'], 250000),
-  mockCar(new Date('2018-05-15'), 'Nissan', ['Eva Martorell'], 350000),
+  mockCar(
+    'id-2',
+    new Date('2019-05-05'),
+    mockManufacturer(),
+    [mockOwner()],
+    250000,
+  ),
+  mockCar(
+    'id-3',
+    new Date('2018-05-15'),
+    mockManufacturer(),
+    [mockOwner('Eva Martorell', new Date('2020-05-27'))],
+    350000,
+  ),
 ];
 
 describe('CarsService', () => {
   let service: CarsService;
+  let repository: CarsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        CarsRepository,
-        OwnersRepository,
-        {
-          provide: getModelToken('Owner'),
-          useValue: {
-            new: jest.fn().mockResolvedValue(mockOwner()),
-            constructor: jest.fn().mockResolvedValue(mockOwner()),
-            find: jest.fn(),
-            findOne: jest.fn(),
-            update: jest.fn(),
-            create: jest.fn(),
-            remove: jest.fn(),
-            exec: jest.fn(),
-          },
-        },
         CarsService,
+        CarsRepository,
         {
           provide: getModelToken('Car'),
           useValue: {
@@ -44,13 +44,25 @@ describe('CarsService', () => {
             findOne: jest.fn(),
             update: jest.fn(),
             create: jest.fn(),
+            deleteOne: jest.fn(),
+            findManufacturer: jest.fn(),
+          },
+        },
+        OwnersRepository,
+        {
+          provide: getModelToken('Owner'),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockOwner()),
+            constructor: jest.fn().mockResolvedValue(mockOwner()),
             remove: jest.fn(),
+            exec: jest.fn(),
           },
         },
       ],
     }).compile();
 
     service = module.get<CarsService>(CarsService);
+    repository = module.get<CarsRepository>(CarsRepository);
   });
 
   afterEach(() => jest.clearAllMocks());
@@ -61,28 +73,47 @@ describe('CarsService', () => {
 
   it('should return all Cars', async () => {
     jest
-      .spyOn(service, 'findAll')
-      .mockImplementation(jest.fn().mockResolvedValueOnce(carsMockArray));
+      .spyOn(repository, 'find')
+      .mockImplementationOnce(() => Promise.resolve(carsMockArray) as any);
     const cars = await service.findAll();
 
-    expect(cars).toBe(carsMockArray);
+    expect(cars).toStrictEqual(carsMockArray);
   });
 
   it('should return a Car', async () => {
-    const carMock = mockCar();
+    const { price, manufacturer, owners, firstRegistrationDate } = mockCar();
+    const mockedCar = {
+      price,
+      manufacturer,
+      owners,
+      firstRegistrationDate,
+    };
+
     jest
-      .spyOn(service, 'findById')
-      .mockImplementation(jest.fn().mockResolvedValueOnce(carMock));
+      .spyOn(repository, 'findOne')
+      .mockImplementationOnce(() => Promise.resolve(mockedCar as any));
 
     const car = await service.findById('some-mocked-id');
 
-    expect(car).toBe(carMock);
+    expect(mockedCar).toStrictEqual(car);
+  });
+
+  it('should create a Car', async () => {
+    jest.spyOn(repository, 'create').mockResolvedValueOnce({
+      id: 'mocked-uuid',
+    } as any);
+
+    const car = await service.create({
+      manufacturer: 'mocked-manufacturer-uuid',
+      firstRegistrationDate: new Date('2020-05-04'),
+      owners: ['mocked-owner-uuid'],
+    });
+
+    expect(car).toStrictEqual({ id: 'mocked-uuid' });
   });
 
   it('should remove a Car', async () => {
-    jest
-      .spyOn(service, 'removeCar')
-      .mockImplementation(jest.fn().mockResolvedValueOnce(true));
+    jest.spyOn(repository, 'delete').mockResolvedValueOnce(true);
 
     const returnedValue = await service.removeCar('some-mocked-id');
 
@@ -91,12 +122,15 @@ describe('CarsService', () => {
 
   it('should return the Car manufacturer', async () => {
     const mockedManufacturer = mockManufacturer();
+
     jest
-      .spyOn(service, 'findManufacturer')
-      .mockImplementation(jest.fn().mockResolvedValueOnce(mockedManufacturer));
+      .spyOn(repository, 'findManufacturer')
+      .mockImplementationOnce(() =>
+        Promise.resolve(mockedManufacturer as Manufacturer),
+      );
 
     const manufacturer = await service.findManufacturer('some-mocked-id');
 
-    expect(manufacturer).toBe(mockedManufacturer);
+    expect(manufacturer).toStrictEqual(mockedManufacturer);
   });
 });
